ad055a46f2721ef8805420d01583cb0ccd1c04b7
Madison Cagle <78162094+mac3141@users.noreply.github.com>
2022-01-29 20:33:38 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+
+const hostname = "localhost";
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/home.html');
+});
+
+// Query String Parameters
+// /greet?name=jane+doe
+app.get('/greet', (req, res) => {
+    let name = req.query.name.charAt(0).toUpperCase() + req.query.name.slice(1);
+    let year = req.query.year;
+    res.send(`<h1>Hello, ${name}!</h1>
+    <h3>You are ${2022 - year - 1} or ${2022 - year} years old.</h3>`);
+});
+
+// Route Parameters
+// /math/num1/operation/num2
+app.get('/math/:num1/:op/:num2', (req, res) => {
+    let num1 = parseInt(req.params.num1);
+    let num2 = parseInt(req.params.num2);
+    let operation = req.params.op;
+
+    switch (operation) {
+        case "plus":
+            res.send(`<h1>${num1 + num2}</h1>`);
+            break;
+        case "minus":
+            res.send(`<h1>${num1 - num2}</h1>`);
+            break;
+        case "times":
+            res.send(`<h1>${num1 * num2}</h1>`);
+            break;
+        case "divide":
+            res.send(`<h1>${num1 / num2}</h1>`);
+            break;
+        default:
+            res.send("Get help");
+            break;
+    }
+});
+
+
+app.listen(port, () => {
+    console.log(`Listening on http://${hostname}:${port} ...`);
+});
