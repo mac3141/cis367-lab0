893d7860c845d3480ce200e568f069df86d2a741
Madison Cagle <78162094+mac3141@users.noreply.github.com>
2022-01-31 16:41:19 -0600

    Visual improvements

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ app.use(express.static(__dirname + '/public'));
 // Query String Parameters
 // /greet?name=jane+doe
 app.get('/greet', (req, res) => {
-    let name = req.query.name.charAt(0).toUpperCase() + req.query.name.slice(1);
+    let name = req.query.name;
     let year = req.query.year;
-    res.send(`<h1>Hello, ${name}!</h1>
+    res.send(`<h1 style="text-transform:capitalize;">Hello, ${name}!</h1>
     <h3>You are ${2022 - year - 1} or ${2022 - year} years old.</h3>`);
 });
 
@@ -23,16 +23,16 @@ app.get('/math/:num1/:op/:num2', (req, res) => {
 
     switch (operation) {
         case "plus":
-            res.send(`<h1>${num1 + num2}</h1>`);
+            res.send(`<h1>${num1} + ${num2} = ${num1 + num2}</h1>`);
             break;
         case "minus":
-            res.send(`<h1>${num1 - num2}</h1>`);
+            res.send(`<h1>${num1} - ${num2} = ${num1 - num2}</h1>`);
             break;
         case "times":
-            res.send(`<h1>${num1 * num2}</h1>`);
+            res.send(`<h1>${num1} * ${num2} = ${num1 * num2}</h1>`);
             break;
         case "divide":
-            res.send(`<h1>${num1 / num2}</h1>`);
+            res.send(`<h1>${num1} / ${num2} = ${num1 / num2}</h1>`);
             break;
         default:
             res.send("Get help");
